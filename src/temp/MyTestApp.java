7dd6a2f27a028edc0bb20908819e819b8e92b926
user1 <user1@124.153.166.154>
2024-02-05 20:03:03 +0900

    안녕 222 추가

--- src/temp/MyTestApp.java
+++ src/temp/MyTestApp.java
@@ -5,7 +5,7 @@ public class MyTestApp {
 	public static void main(String[] args) {
 		System.out.printf("Hello World 456\n");
 		System.out.printf("안녕 789\n");
-
+		System.out.printf("안녕 222\n");
 	}
 
 }
