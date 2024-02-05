cbd1cfadebe21118bbabc3dd3ba44ab1de16d99b
user1 <user1@124.153.166.154>
2024-02-05 19:52:05 +0900

    소스 수정

--- src/temp/MyTestApp.java
+++ src/temp/MyTestApp.java
@@ -3,7 +3,8 @@ package temp;
 public class MyTestApp {
 
 	public static void main(String[] args) {
-		System.out.printf("Hello World 456");
+		System.out.printf("Hello World 456\n");
+		System.out.printf("안녕 789\n");
 
 	}
 
